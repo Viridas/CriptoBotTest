639cfdef39f2cb8faee42d05ff815bcac37386db
Urii Lutsenko <dlyawarfeisa@gmail.com>
2024-10-15 15:24:02 +0300

    Changes

--- Program.cs
+++ Program.cs
@@ -31,6 +31,7 @@ class Program
     private static Dictionary<long, Message> UserMessage = new Dictionary<long, Message>();
     static async Task Main(string[] args)
     {
+        //Start
         currencies.Add("Tether, USDT");
         currencies.Add("Monobank, UAH");
         currencies.Add("ПриватБанк, UAH");

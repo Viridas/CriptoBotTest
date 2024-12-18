190d9a56e6b77c82462709076fb0957d7b5e89b4
Urii Lutsenko <dlyawarfeisa@gmail.com>
2024-12-18 12:15:31 +0200

    Change .NET

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using System.Text.RegularExpressions;
 using CryptoBot;
 using CryptoBot.Services;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.VisualBasic;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;

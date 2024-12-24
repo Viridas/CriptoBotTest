af3b9446d3eaaef1de71fe4b394b97ae38bc8473
Urii Lutsenko <dlyawarfeisa@gmail.com>
2024-12-24 13:33:14 +0200

    Initial commit for new repository

--- obj/Debug/net9.0/CryptoBot.AssemblyInfo.cs
+++ obj/Debug/net9.0/CryptoBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CryptoBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e744826443f6492cffe8a3157763287aa9d90b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+190d9a56e6b77c82462709076fb0957d7b5e89b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CryptoBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CryptoBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
